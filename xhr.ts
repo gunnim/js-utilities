7ae42600ed9bd540f41286c371d8759afae42e42
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-02-20 13:54:16 +0000

    updated to newest version

--- xhr.ts
+++ xhr.ts
@@ -1,4 +1,8 @@
-﻿
+﻿/*
+ * XHR Wrapper
+ * Gunnar Már Óttarsson 2017
+ */
+
 export interface P1 {
     type?: string;
     method?: string;
@@ -19,114 +23,82 @@ export default function constructXhr(p1 : P1 | string,
 
     var xhr = new XMLHttpRequest();
 
+    // Wrapper function that encapsulates the consumers onload callback
+    // Used to fix older IE incompatibility with xhr.responseType = 'json'
+    var onloadWrapper = function(e : Event) {
+
+        var xhr = e.target as XMLHttpRequest;
+        
+                                            // Arrays typeof == object also 
+        if (!xhr.responseType.length || typeof xhr.response != 'object') {
+
+            var newXhr : XhrLike = {
+                response: JSON.parse(xhr.response),
+                status: xhr.status
+            };
+
+            // If the callback parameter was left out it must be in the P1 obj
+            callback ? callback(newXhr) : (p1 as P1).callback(newXhr);
+        }
+        else
+            callback ? callback(xhr) : (p1 as P1).callback(xhr);
+    };
+
     if (typeof p1 === 'object') {
       
       var type = p1.type || p1.method || 'POST';
 
       xhr.open(type, p1.url);
-      xhr.responseType = p1.responseType ? p1.responseType : 'json';
+      xhr.responseType = p1.responseType || 'json';
 
-      // Use the default request header when transmitting MIME encoded data
-      // Usage: Specifiy the contentType explicitly with p1.contentType
+      // Use the default request header when transmitting multipart MIME encoded data
+      // Usage: Specify the contentType explicitly with p1.contentType
       // set no flags to use our default of application/x-www-form-urlencoded
       // set p1.defaultContentType to use XHR default of multipart/form-data
       // when transmitting files
       if (!p1.defaultContentType)
-          xhr.setRequestHeader('Content-type', p1.contentType ? p1.contentType : 'application/x-www-form-urlencoded');
-
-      xhr.onload = function() {
-
-        // IE Fix
-        if (!xhr.responseType.length) {
-
-            var newXhr = {
-                response: JSON.parse(xhr.response),
-                status: xhr.status
-            };
-
-            callback ? callback(newXhr) : p1.callback(newXhr);
-        }
-        else if (typeof xhr.response != 'object') { // Arrays typeof == object also 
-        
-            var newXhr = {
-                response: JSON.parse(xhr.response),
-                status: xhr.status
-            };
-
-            callback ? callback(newXhr) : p1.callback(newXhr);
-        }
-        else
-            callback ? callback(xhr) : p1.callback(xhr);
-      };
+          xhr.setRequestHeader('Content-type', p1.contentType ||
+												'application/x-www-form-urlencoded');
     }
     else {
-      xhr.open('POST', p1);
-      xhr.responseType = 'json';
-      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-      xhr.onload = function() {
-
-            // IE Fix
-            if (!xhr.responseType.length) {
-
-                var newXhr = {
-                    response: JSON.parse(xhr.response),
-                    status: xhr.status
-                };
-
-                callback(newXhr);
-            }
-            else if (typeof xhr.response != 'object') { // Arrays typeof == object also 
-            
-                var newXhr = {
-                    response: JSON.parse(xhr.response),
-                    status: xhr.status
-                };
-
-                callback(newXhr);
-            }
-            else
-                callback(xhr);
-        };
+		xhr.open('POST', p1);
+		xhr.responseType = 'json';
+		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     }
 
-    return xhr;
+	xhr.onload = onloadWrapper;
 
-    // Demo onload callback
-    /*
-        var myCallback = function () {
-                                                      // xhr.response.success is a custom response we might return from our controller
-            if (xhr.status >= 200 && xhr.status < 300 && xhr.response.success) {
-                console.log('success');
-            }
-            else console.log('error');
-
-            AlwaysRunThis();
-        };
-    */
-
-    // Simple example usage - send application/x-www-form-urlencoded with POST
-    /*
-        var formData = 'param1=' + encodeURIComponent(paramStr1) + '&param2=' + encodeURIComponent(paramStr2);
-
-        var constructXhr = require('gUtilities/xhr.ts');
-        var xhr = constructXhr('/umbraco/surface/test/action', myCallback);
-
-        xhr.send(formData);
-    */
-
-    // Example usage - send a js obj
-    /*
-        var myObj = { prop1: 'a', prop2: 1 };
-
-        var constructXhr = require('gUtilities/xhr.ts');
-        var xhr = constructXhr({
-            type: 'POST',
-            url: '/umbraco/surface/test/action',
-            responseType: 'json',
-            contentType: 'application/json',
-        }, myCallback);
-
-        xhr.send(JSON.stringify(myObj));
-    */
+	return xhr;
 };
+
+// Demo onload callback
+/*
+	var myCallback = xhr => {
+		if (xhr.status >= 200 && xhr.status < 300) {
+			console.log('success');
+		}
+		else console.log('error');
+		AlwaysRunThis();
+	};
+*/
+
+// Simple example usage - send application/x-www-form-urlencoded with POST
+/*
+	var formData = 'param1=' + encodeURIComponent(paramStr1) + '&param2=' + encodeURIComponent(paramStr2);
+	var constructXhr = require('utilities/xhr.ts');
+	var xhr = constructXhr('/umbraco/surface/test/action', myCallback);
+	xhr.send(formData);
+*/
+
+// Example usage - send a js obj
+/*
+	var myObj = { prop1: 'a', prop2: 1 };
+	var constructXhr = require('utilities/xhr.ts');
+	var xhr = constructXhr({
+		type: 'POST',
+		url: '/umbraco/surface/test/action',
+		responseType: 'json',
+		contentType: 'application/json',
+	}, myCallback);
+	xhr.send(JSON.stringify(myObj));
+*/
