11c9e8744e45fe97e5a2c3aaf29650229973864d
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-04-21 21:29:07 +0000

    commenting for isObjEmpty func

--- isObjEmpty.ts
+++ isObjEmpty.ts
@@ -2,6 +2,9 @@
  *  Is Object Empty Helper
  * ======================== */
 
+/**
+ * True if empty
+ */
 export default function isObjEmpty(obj : Object) {
 
     if (!obj || obj && Object.keys(obj).length === 0 && obj.constructor === Object)
