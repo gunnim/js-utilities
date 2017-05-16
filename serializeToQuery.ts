3bfbd698c5b7d4f0ab1127b86def7bc843a1b0d5
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-05-16 19:13:19 +0000

    formParsing

---
+++ serializeToQuery.ts
@@ -0,0 +1,10 @@
+var serializeToQuery = function (obj) {
+    var str = [];
+    for (var p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    return str.join("&");
+}
+
+export default serializeToQuery;
