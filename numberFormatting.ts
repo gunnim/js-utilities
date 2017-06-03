f34edb1d494209303fb416a547bc51d93390879a
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-06-03 22:37:42 +0000

    numberFormatting

---
+++ numberFormatting.ts
@@ -0,0 +1,10 @@
+/**
+ * Number With Delimiter Helper
+ * https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript?page=1&tab=votes#tab-top
+ * 
+ * @param num 
+ * @param delimiter 
+ */
+export default function(num: number, delimiter: string) {
+  num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, delimiter);
+};
