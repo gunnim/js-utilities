7dc4b22d44567391ca5c2778145aac9638213e1e
Gunnar Már Óttarsson <gunni@starcraft.is>
2019-09-15 21:54:31 +0000

    randomString

---
+++ randomString.ts
@@ -0,0 +1,8 @@
+export default function randomString(length = 15) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
